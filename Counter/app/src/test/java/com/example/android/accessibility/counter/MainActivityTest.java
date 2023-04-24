fd1bfbea1f214801c3155f504312fdd2b8982de7
Anderson Garcia <andersongarcia@usp.br>
2023-04-24 16:18:32 -0300

    Configure AATK for Counter App

--- Counter/app/src/test/java/com/example/android/accessibility/counter/MainActivityTest.java
+++ Counter/app/src/test/java/com/example/android/accessibility/counter/MainActivityTest.java
@@ -1,4 +1,43 @@
 package com.example.android.accessibility.counter;
 
+import android.view.View;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import br.usp.icmc.ppgccmc.accessibility_tests.mars.TestAdequateContrastRatio;
+import br.usp.icmc.ppgccmc.accessibility_tests.mars.TestTouchTargetSize;
+import br.usp.icmc.ppgccmc.accessibility_tests.runners.AccessibilityTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
+    private View rootView;
+    private AccessibilityTestRunner runner;
+
+    @Rule
+    public ErrorCollector collector = new ErrorCollector();
+
+    @Before
+    public void setUp() {
+        MainActivity activity = Robolectric.buildActivity(MainActivity.class).create().get();
+
+        // Get the root node of the view hierarchy
+        rootView = activity.getWindow().getDecorView().getRootView();
+        runner = new AccessibilityTestRunner(collector);
+    }
+
+    @Test
+    public void mustUseAdequateContrastRatio(){
+        runner.runAccessibilityTest(rootView, new TestAdequateContrastRatio());
+    }
+
+    @Test
+    public void mustTouchTargetBeLargeEnough(){
+        runner.runAccessibilityTest(rootView, new TestTouchTargetSize());
+    }
 }
